6f2d574cd5e07c30cb7caf0eb565827cb6c280ea
rylan.davis <superrylan08@gmail.com>
2023-05-24 15:25:12 -0500

    Added another format

--- 1PlayerVersion.py
+++ 1PlayerVersion.py
@@ -5,6 +5,17 @@ player = input("Rock, Paper, or Scissors?: ")
 bot = random.choice(list1)
 
 
+if player == "Rock" and bot == "Scissors" or player == "Scissors" and bot == "Paper" or player == "Paper" and bot == "Rock":
+    print("Bot chose", bot, " and you win!")
+elif player == "Rock" and bot == "Paper" or player == "Scissors" and bot == "Rock" or player == "Paper" and bot == "Scissors":
+    print("Bot chose", bot , "and wins!")
+elif player == "Rock" and bot == "Rock" or player == "Scissors" and bot == "Scissors" or player == "Paper" and bot == "Paper":
+    print("Its a tie!")
+else:
+    print("Something went wrong!")
+
+
+"""
 if player == "Rock":
     if bot == "Scissors":
         print("You win!")
@@ -29,9 +40,8 @@ elif player == "Scissors":
 else:
     print("Something went wrong!")
 
-# Better looking code /\
-# Less good looking code \/
-"""
+    
+
 if player == "Rock" and bot == "Scissors":
     print("You win!")
 elif player == "Rock" and bot == "Paper":
@@ -52,4 +62,5 @@ elif player == "Scissors" and bot == "Scissors":
     print("Its a tie!")        
 else:
     print("You did not enter correctly")
+
 """
